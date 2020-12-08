bb124e9f111da26424ace4b0f335c9bd7e6a5c5a
Solannae <16961033+Solannae@users.noreply.github.com>
2020-12-08 21:06:35 +0100

    Day 6

---
+++ AdventOfCode-2020/Archive/Day6.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode_2020.Archive
+{
+    class Day6
+    {
+        public static void Run()
+        {
+            const string path = @"../../../Inputs/day6.txt";
+
+            using (var stream = new StreamReader(path))
+            {
+                string line;
+                var sumYes = 0;
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    List<HashSet<char>> currentGroupYes = new List<HashSet<char>>();
+
+                    do
+                    {
+                        HashSet<char> currentPerson = new HashSet<char>();
+
+                        foreach (var c in line)
+                        {
+                            currentPerson.Add(c);
+                        }
+
+                        currentGroupYes.Add(currentPerson);
+
+                        line = stream.ReadLine();
+                    } while (line != null && line != string.Empty);
+
+                    foreach (var answer in currentGroupYes[0])
+                    {
+                        var similarAnswers = 0;
+
+                        for (var i = 1; i < currentGroupYes.Count; ++i)
+                        {
+                            if (currentGroupYes[i].Contains(answer))
+                                ++similarAnswers;
+                        }
+
+                        if (similarAnswers == currentGroupYes.Count - 1)
+                            ++sumYes;
+                    }
+                }
+
+                Console.WriteLine("Number of questions to which everyone answered yes in groups: " + sumYes);
+            }
+        }
+    }
+}
